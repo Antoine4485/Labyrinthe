09c44194fae43c5e4bb5bce02d7000275626dccb
Antoine4485 <pzx.antoine@gmail.com>
2024-08-05 17:21:55 +0200

    # si le point de départ se situe dans un cul-de-sac, on n'y revient pas

--- labyrinthe.py
+++ labyrinthe.py
@@ -11,18 +11,44 @@ class Labyrinth(QtW.QWidget):
 
     def __init__(self, grid):
         super().__init__()
-        # grille pour l'affichage
-        self._initial_grid = grid
-        # grille pour les traitements (les valeurs de cette grille sont modifiées dans les "culs-de-sacs" pour ne pas
-        # y retourner si on y est allé une fois)
-        self._grid = copy.deepcopy(grid)
-        self._path = []
         self._path_value = 0
         self._wall_value = 1
         self._start_value = 2
         self._finish_value = 3
         self._timer_delay = 0.2
+        # grille pour l'affichage
+        self._grid = [[{"value": value,
+                        "authorisation": False if value == self._wall_value else True,
+                        "label": self._get_label_by_value(value)} for value in row] for row in grid]
+
+        #self._initial_grid = grid
+        # grille pour les traitements (les valeurs de cette grille sont modifiées dans les "culs-de-sacs" pour ne pas
+        # y retourner si on y est allé une fois)
+        #self._grid = copy.deepcopy(grid)
+        self._path = []
+        #self.labels = []
         self._show_grid()
+
+    def _get_label_by_value(self, value):
+        label = QtW.QLabel()
+
+        match value:
+            case self._start_value:
+                label.setText("D")
+            case self._finish_value:
+                label.setText("A")
+            case self._wall_value:
+                label.setPixmap(QPixmap('mur.webp'))
+
+        return label
+
+    # def _get_cell_text_by_value(self, value):
+    #     match value:
+    #         case self._start_value:
+    #             return "D"
+    #         case self._finish_value:
+    #             return "A"
+    #     return ""
 
     def _get_and_show_path(self):
         # on ajoute la position de départ à la liste
@@ -31,26 +57,77 @@ class Labyrinth(QtW.QWidget):
         while True:
             # quand on est sur une case, on crée une liste des coordonnées des cases autour qui contiennent un 0 ou un 3
             # et dont la case d'où l'on vient ne fait pas partie
-            next_pos = self._get_next_pos()
+            #next_pos = self._get_next_pos()
             # s'il n'y a aucune case possible (on est dans un cul-de-sac), on met un 1 sur la case où on est pour ne pas
             # y retourner, on la supprime de la liste et on revient en arrière d'une case
-            if not next_pos:
-                self._timer()
-                current_pos = self._path[-1]
-                self._grid[current_pos[0]][current_pos[1]] = self._wall_value
-                self._remove_last_pos()
-                # current_pos = self._path[-1]
-                # self.labels[current_pos[0]][current_pos[1]].setPixmap(QPixmap('bb.png'))
-                continue
+            self._timer()
+            possible_next_positions = self._get_possible_next_positions()
+            prec_pos = self._path[-2] if len(self._path) > 1 else None
+            #if (pos != prec_pos):
+            match len(possible_next_positions):
+                case 0:
+                    return
+                case 1:
+                    next_pos = possible_next_positions[0]
+                    current_pos = self._path[-1]
+                    self._grid[current_pos[0]][current_pos[1]]["authorisation"] = False
+                    # cas où on est dans un cul-de-sac
+                    if next_pos == prec_pos:
+                        self._remove_last_pos()
+                        continue
+                #case 2:
+                    # s'il y a deux possibilités (avancer ou reculer), on avance
+                    # index_prec_pos = possible_next_positions.index(prec_pos)
+                    # index_next_pos = int(not index_prec_pos)
+                    # next_pos = possible_next_positions[index_next_pos]
+                case _:
+                    if prec_pos in possible_next_positions:
+                        possible_next_positions.remove(prec_pos)
+                    #possible_next_positions.remove(prec_pos)
+                    #index_prec_pos = possible_next_positions.index(prec_pos)
+                    next_pos = random.choice(possible_next_positions)
+
+            # if len(possible_next_positions) == 0:
+            #     return
+            #
+            # elif len(possible_next_positions) == 1:
+            #     next_pos = possible_next_positions[0]
+            #     self._timer()
+            #     current_pos = self._path[-1]
+            #     self._grid[current_pos[0]][current_pos[1]]["authorisation"] = False
+            #     # cas où on est dans un cul-de-sac
+            #     if next_pos == prec_pos:
+            #         self._remove_last_pos()
+            #         continue
+            #     # current_pos = self._path[-1]
+            #     # self.labels[current_pos[0]][current_pos[1]].setPixmap(QPixmap('bb.png'))
+            #
+            # elif len(possible_next_positions) == 2:
+            #     index_prec_pos = possible_next_positions.index(prec_pos)
+            #     index_next_pos = int(not index_prec_pos)
+            #     next_pos = possible_next_positions[index_next_pos]
+            #     #current_pos = self._path[-1]
+            #     #self._grid[current_pos[0]][current_pos[1]]["authorisation"] = False
+            # else:
+            #     next_pos = random.choice(possible_next_positions)
+
+            # if not next_pos:
+            #     self._timer()
+            #     current_pos = self._path[-1]
+            #     self._grid[current_pos[0]][current_pos[1]]["authorisation"] = False
+            #     self._remove_last_pos()
+            #     # current_pos = self._path[-1]
+            #     # self.labels[current_pos[0]][current_pos[1]].setPixmap(QPixmap('bb.png'))
+            #     continue
             self._add_pos(next_pos)
             # si la case contient le 3, le programme est terminé
-            if self._grid[next_pos[0]][next_pos[1]] == self._finish_value:
+            if self._grid[next_pos[0]][next_pos[1]]["value"] == self._finish_value:
                 return
 
     def _init_start_pos(self):
         for i in range(len(self._grid)):
             for j in range(len(self._grid[i])):
-                if self._grid[i][j] == self._start_value:
+                if self._grid[i][j]["value"] == self._start_value:
                     self._add_pos((i, j))
                     return
 
@@ -67,19 +144,44 @@ class Labyrinth(QtW.QWidget):
         self._timer()
         if len(self._path) > 0:
             current_pos = self._path[-1]
-            self.labels[current_pos[0]][current_pos[1]].clear()
+            current_label = self._grid[current_pos[0]][current_pos[1]]["label"]
+            #label_text = current_label.text()
+            current_label.clear()
+            if self._grid[current_pos[0]][current_pos[1]]["value"] == self._start_value:
+                current_label.setText("D")
             #self.labels[current_pos[0]][current_pos[1]].setStyleSheet(f"background-color: rgb(200, 200, 200);")
         self._path.append(pos)
-        self.labels[pos[0]][pos[1]].setPixmap(QPixmap('bb.png'))
+        self._grid[pos[0]][pos[1]]["label"].setPixmap(QPixmap('bb.png'))
         #self.labels[pos[0]][pos[1]].setPixmap(QPixmap())
         #self.labels[pos[0]][pos[1]].setStyleSheet(f"background-color: rgb(200, 200, 200);")
 
     def _remove_last_pos(self):
+        self._timer()
+        # s'il n'y a qu'un élément c'est forcément le point de départ, donc on ne l'enlève pas
+        if len(self._path) == 1:
+            return
         pos = self._path.pop()
-        self.labels[pos[0]][pos[1]].clear()
-        last_pos = self._path[-1]
-        self.labels[last_pos[0]][last_pos[1]].setPixmap(QPixmap('bb.png'))
+        label = self._grid[pos[0]][pos[1]]["label"]
+        label.clear()
+        if self._grid[pos[0]][pos[1]]["value"] == self._start_value:
+            label.setText("D")
+        if len(self._path) > 0:
+            last_pos = self._path[-1]
+            self._grid[last_pos[0]][last_pos[1]]["label"].setPixmap(QPixmap('bb.png'))
         #self.labels[pos[0]][pos[1]].setStyleSheet(f"background-color: none;")
+
+    def _get_possible_next_positions(self):
+        final_possible_next_pos = []
+        #prec_pos = self._path[-2] if len(self._path) > 1 else None
+
+        for pos in self._get_closest_positions():
+            #if (pos != prec_pos and
+            if (pos[0] in range(len(self._grid)) and
+                    pos[1] in range(len(self._grid[pos[0]])) and
+                    self._grid[pos[0]][pos[1]]["authorisation"] is True):
+                final_possible_next_pos.append(pos)
+
+        return final_possible_next_pos
 
     def _get_next_pos(self) -> None | tuple:
         final_possible_next_pos = []
@@ -89,7 +191,7 @@ class Labyrinth(QtW.QWidget):
             if (pos != prec_pos and
                     pos[0] in range(len(self._grid)) and
                     pos[1] in range(len(self._grid[pos[0]])) and
-                    self._grid[pos[0]][pos[1]] != self._wall_value):
+                    self._grid[pos[0]][pos[1]]["authorisation"] is True):
                 final_possible_next_pos.append(pos)
 
         if len(final_possible_next_pos) == 0:
@@ -105,27 +207,13 @@ class Labyrinth(QtW.QWidget):
         # Création d'un groupe de widgets
         group = QtW.QGroupBox()
 
-        self.labels = [[QtW.QLabel() for _ in range(len(self._initial_grid[i]))] for i in range(len(self._initial_grid))]
+        #self.labels = [[QtW.QLabel() for _ in range(len(self._initial_grid[i]))] for i in range(len(self._initial_grid))]
 
-        for i in range(len(self._initial_grid)):
-            for j in range(len(self._initial_grid[i])):
-                value = self._initial_grid[i][j]
-                if value == self._wall_value:
-                    label = QtW.QLabel()
-                    label.setPixmap(QPixmap('mur.webp'))
-                    #label.setStyleSheet(f"background-color: red;")
-                else:
-                    match value:
-                        case self._path_value:
-                            value = ""
-                        case self._start_value:
-                            value = "D"
-                        case self._finish_value:
-                            value = "A"
-                    label = QtW.QLabel(str(value))
+        for i in range(len(self._grid)):
+            for j in range(len(self._grid[i])):
+                label = self._grid[i][j]["label"]
                 label.setFixedSize(30, 30)
                 label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-                self.labels[i][j] = label
                 grid_layout.addWidget(label, i, j)
 
         # On définit la grille comme disposition du groupe
@@ -146,9 +234,9 @@ class Labyrinth(QtW.QWidget):
     def _get_closest_positions(self):
         current_pos = self._path[-1]
         return ((current_pos[0] - 1, current_pos[1]),
-                 (current_pos[0], current_pos[1] + 1),
-                 (current_pos[0] + 1, current_pos[1]),
-                 (current_pos[0], current_pos[1] - 1))
+                (current_pos[0], current_pos[1] + 1),
+                (current_pos[0] + 1, current_pos[1]),
+                (current_pos[0], current_pos[1] - 1))
 
     def _timer(self):
         time.sleep(self._timer_delay)
@@ -161,17 +249,17 @@ if __name__ == '__main__':
     #     [1, 1, 0, 1, 0, 1, 1, 1, 1, 1],
     #     [0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
     #     [0, 1, 0, 1, 0, 0, 1, 1, 1, 1],
-    #     [1, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+    #     [1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
     #     [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
     #     [1, 1, 0, 1, 1, 1, 1, 0, 0, 0],
     #     [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
     # ]
 
     # grid = [
-    #     [1, 1, 1, 1, 0, 1, 0, 1, 1, 1],
+    #     [1, 1, 1, 1, 0, 1, 0, 1, 3, 1],
     #     [1, 2, 0, 1, 0, 0, 0, 0, 0, 1],
-    #     [1, 1, 0, 1, 0, 1, 1, 1, 1, 1],
-    #     [0, 1, 0, 1, 0, 0, 0, 0, 0, 3],
+    #     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    #     [0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
     #     [0, 1, 0, 1, 0, 0, 1, 1, 1, 1],
     #     [1, 1, 0, 1, 1, 0, 1, 0, 0, 0],
     #     [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
@@ -179,25 +267,22 @@ if __name__ == '__main__':
     #     [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
     # ]
 
-    # grid = [
-    #     [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #     [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    #     [1, 2, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
-    #     [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 3],
-    #     [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1],
-    #     [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-    #     [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    #     [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
-    #     [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
-    #     [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-    #     [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-    # ]
+    grid = [
+        [1, 1, 1, 1, 0, 1, 0, 1, 1, 1],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 0, 1, 1, 1, 1, 1],
+        [0, 1, 2, 1, 0, 0, 0, 0, 0, 3],
+        [0, 1, 0, 1, 0, 0, 1, 1, 1, 1],
+        [1, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [1, 1, 0, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    ]
 
     grid = [
         [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 2, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 3],
         [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1],
         [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
@@ -205,21 +290,36 @@ if __name__ == '__main__':
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
         [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
         [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-        [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 2, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1],
-        [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1],
-        [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-        [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
         [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0]
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
     ]
+
+    # grid = [
+    #     [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    #     [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 3],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0],
+    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    #     [1, 2, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0],
+    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    #     [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
+    #     [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+    #     [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0]
+    # ]
 
     # Création de l'application
     app = QtW.QApplication([])
